e79debc03a2c5d3265f7c0f6ed2697a0abab3faa
Wilfredo Lopez <jrlopez85@gmail.com>
2018-06-16 10:20:20 -0700

    Initial Commit

---
+++ mwCam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mwCam
+//
+//  Created by Dynmk Studio on 6/16/18.
+//  Copyright © 2018 Dynmk Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
